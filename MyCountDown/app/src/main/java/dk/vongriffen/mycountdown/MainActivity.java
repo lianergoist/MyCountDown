131adad3a75aa5400ab7930657a20940d85685a5
Thomas Jensen <lianergoist@vongriffen.dk>
2017-06-24 12:02:15 +0200

    First commit

---
+++ MyCountDown/app/src/main/java/dk/vongriffen/mycountdown/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.vongriffen.mycountdown;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
