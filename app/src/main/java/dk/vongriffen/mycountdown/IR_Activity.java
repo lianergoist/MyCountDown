84f27828da46983e709edd0a1ab968335f1c8373
Thomas Jensen <lianergoist@vongriffen.dk>
2017-10-05 23:40:53 +0200

    fixed import

--- app/src/main/java/dk/vongriffen/mycountdown/IR_Activity.java
+++ app/src/main/java/dk/vongriffen/mycountdown/IR_Activity.java
@@ -14,6 +14,8 @@ import android.support.v4.app.*;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.*;
 import android.os.PowerManager.*;
+import android.view.View.*;
+
 
 public class IR_Activity extends AppCompatActivity implements IR_EditDialogListener, IR_AddDialogListener
 {
